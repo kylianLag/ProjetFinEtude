d9689eddc555161bf75d47b0ae0b2832659498a0
Kiddo-Mashiro <99025818+Kiddo-Mashiro@users.noreply.github.com>
2024-11-27 14:40:58 +0100

    Add files via upload

---
+++ projet/navbar.php
@@ -0,0 +1,23 @@
+<header class="header">
+    <div class="logo">
+        <img src="assets/img/logo.png">
+    </div>
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="#exercices">S'exercer</a></li>
+            <li><a href="ressources.php">Ressources</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        </nav>
+        <div class="auth-buttons">
+            <?php 
+                if(isset($_SESSION['connexion']) && !$_SESSION['connexion']?? ""){
+                }
+            ?>
+            
+            <a href='inscription.php' class='button-link2'>S'inscrire</a>
+                    <a href='connexion.php' class='button-link'>Connexion</a>
+        </div>
+    </header>
