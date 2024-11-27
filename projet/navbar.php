02aa01df500e7cc132e4116ec952daf35df7f087
kylianLag <135212191+kylianLag@users.noreply.github.com>
2024-11-27 14:43:54 +0100

    Update navbar.php

--- projet/navbar.php
+++ projet/navbar.php
@@ -1,15 +1,16 @@
 <header class="header">
-    <div class="logo">
-        <img src="assets/img/logo.png">
-    </div>
-    <nav class="navbar">
-        <ul class="nav-list">
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="dashboard.php">Dashboard</a></li>
-            <li><a href="#exercices">S'exercer</a></li>
-            <li><a href="ressources.php">Ressources</a></li>
-            <li><a href="contact.php">Contact</a></li>
-        </ul>
+        <div class="logo">
+            <img id ="logoImg" src="logo.png">
+        </div>
+        <script type="text/javascript" src="script.js"></script>
+        <nav class="navbar">
+            <ul class="nav-list">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="#exercices">S'exercer</a></li>
+                <li><a href="ressources.php">Ressources</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
         </nav>
         <div class="auth-buttons">
             <?php 
@@ -18,6 +19,6 @@
             ?>
             
             <a href='inscription.php' class='button-link2'>S'inscrire</a>
-                    <a href='connexion.php' class='button-link'>Connexion</a>
+            <a href='connexion.php' class='button-link'>Connexion</a>
         </div>
     </header>
